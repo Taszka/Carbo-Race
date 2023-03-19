093f29b321ae293eecb2d63f1ea439ab4b77e374
Taszka <jjakub.romanowski@gmail.com>
2023-03-19 22:24:03 +0100

    Our JS works

--- Back/JavaScript/button_start.js
+++ Back/JavaScript/button_start.js
@@ -1,4 +1,7 @@
  // Funkcja uruchamiająca animację po kliknięciu przycisku
-function startAnimation() {
+function startAnimationAndRedirect() {
   document.getElementById("block_left").style.animationPlayState = "running";
+  document.getElementById("block_right").style.animationPlayState = "running";
+  window.location.href = "../../Front/HTML_Templates/Sowa_require.html";
 }
+//  window.location.href = 'Carbo Race/Front/HTML_Templates/Sowa-require.html';
